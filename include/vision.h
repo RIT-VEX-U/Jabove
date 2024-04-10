16f4e4c104db96f41d558f1b93455cba3f3b7ecf
max-49 <massimomarino49@gmail.com>
2024-04-09 21:57:04 -0400

    vision sensor setup

---
+++ include/vision.h
@@ -0,0 +1,9 @@
+#include "vex.h"
+extern vex::vision::signature SIG_1;
+extern vex::vision::signature SIG_2;
+extern vex::vision::signature SIG_3;
+extern vex::vision::signature SIG_4;
+extern vex::vision::signature SIG_5 ;
+extern vex::vision::signature SIG_6;
+extern vex::vision::signature SIG_7 ;
+extern vex::vision cam;
---
+++ src/vision.cpp
@@ -0,0 +1,11 @@
+/*vex-vision-config:begin*/
+#include "vex.h"
+vex::vision::signature TRIBALL = vex::vision::signature (1, -5739, -5061, -5400, -3795, -2895, -3345, 7.5, 0);
+vex::vision::signature SIG_2 = vex::vision::signature (2, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_3 = vex::vision::signature (3, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_4 = vex::vision::signature (4, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_5 = vex::vision::signature (5, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_6 = vex::vision::signature (6, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_7 = vex::vision::signature (7, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision cam = vex::vision (vex::PORT8, 69, TRIBALL, SIG_2, SIG_3, SIG_4, SIG_5, SIG_6, SIG_7);
+/*vex-vision-config:end*/
